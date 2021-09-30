61ecdf0a24943e6b5c8f7c0876ef8ac402c5abaa
RaulPlanasJover <90848607+RaulPlanasJover@users.noreply.github.com>
2021-09-30 16:51:44 +0200

    Cambios_del_programa
    Se entiende peor!

--- bin2hex.py
+++ bin2hex.py
@@ -1,8 +1,4 @@
-# ------------------------------------------------------
-# Convierte un número binario a hexadecimal.
-# El número binario es un string e.g. "101"
-# El número hexadecimal es también un string e.g. "AB5"
-# ------------------------------------------------------
+
 
 def TablabinHex(cuatro_digitos_binarios):
     digito_hexadecimal = ""
@@ -52,16 +48,13 @@ def bin2hex(numero_binario):
         
     return numero_hexadecimal
 
-# ----------------------------------------
-# MAIN
-# ----------------------------------------
+
 if __name__ == "__main__":
-    # Pide al usuario el número binario a convertir 
-    # Como el número binario es un string, no hace falta usar int()
+    
     numero_binario = input("Escribe el número en binario que quieres convertir: ")
 
-    # se llama a la función bin2hex() para hacer la conversión
+   
     numero_hexadecimal = bin2hex(numero_binario)
 
-    # Muestra por pantalla el resultado.
+    
     print("El numero binario " + numero_binario + " es " + numero_hexadecimal + " en hexadecimal.")
